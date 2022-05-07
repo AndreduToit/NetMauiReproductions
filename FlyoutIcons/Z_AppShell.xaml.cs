e5946f10ab629b1ef9632970ad3b32e38a572fcf
Andre du Toit <dutoita1011@gmail.com>
2022-05-07 13:06:33 +0200

    FlyoutIcons - tried some options

---
+++ FlyoutIcons/Z_AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp1;
+
+public partial class Z_AppShell : Shell
+{
+    public Z_AppShell()
+    {
+        InitializeComponent();
+        BindingContext = new ViewModelAppShell();
+    }
+}
