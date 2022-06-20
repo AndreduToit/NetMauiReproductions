623b66d22e94d72be9614a3abdaaad806963d057
Andre du Toit <dutoita1011@gmail.com>
2022-06-20 09:49:28 +0200

    Add WebAPI

---
+++ WebAPI/Client/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiClient
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            var devSslHelper = new DevHttpsConnectionHelper(sslPort: 7331);
+            var http = devSslHelper.HttpClient;
+            var response = await http.GetStringAsync($"{devSslHelper.DevServerRootUrl }/Home/TestMe");
+
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+            if (!string.IsNullOrEmpty(response))
+                WebAPIBtn.Text = $"Response from WebAPI: {response}";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
---
+++ WebAPI/Server/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace WebAPI.Controllers
+{
+    /// <summary>
+    /// Manages the standard web server pages
+    /// (for testing)
+    /// </summary>
+    public class HomeController : Controller
+    {
+        #region fields
+
+        /// <summary>
+        /// The manager for querying all routes
+        /// </summary>
+        private readonly IActionDescriptorCollectionProvider mActionDescriptorCollectionProvider;
+
+        #endregion
+
+        #region ctors
+
+        public HomeController(
+            IActionDescriptorCollectionProvider tActionDescriptorCollectionProvider)
+        {
+            mActionDescriptorCollectionProvider = tActionDescriptorCollectionProvider;
+        }
+
+        #endregion
+
+        #region index - tests
+        [Route("~/Home/TestMe")]
+        [HttpGet]
+        public IActionResult TestMe()
+        {
+            return Content("Home Controller here!");
+        }
+ 
+        #endregion
+
+
+    }
+
+}
